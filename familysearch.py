eeee6268147045ed0aeb6d420e259fd9f4ee0deb
Gregory <lacrosselogo16@gmail.com>
2019-05-09 03:27:21 -0600

    created main structure for program and .txt file

--- familysearch.py
+++ familysearch.py
@@ -1,4 +1,37 @@
 import pandas as pd
 import xlrd as xl
-superPowers = pd.read_excel (r'Super Powers Descriptor List.xlsx')
-print(superPowers)
+
+def WriteScript(file, commands):
+	f= open(file,"w+")
+	for command in commands:
+		f.write(command + "\n")
+	f.close()
+
+def ShowScript(commands):
+	for command in commands:
+		print(command)
+
+def GenerateCommands(row, commands):
+	commands.append(row["DESCRIPTION"])
+
+def GenerateControlledVocabularyScript(data, listId):
+	scriptCommands = ["update_list	"+ str(listId) +"	lock=false"]
+
+	for lab, row in data.iterrows():
+		GenerateCommands(row, scriptCommands)
+
+	scriptCommands.append("update_list	"+ str(listId) +"	lock=true")
+	return scriptCommands
+
+def ReadSampleData(file):
+	return pd.read_excel(file)
+    
+    
+if __name__ == '__main__':
+	file = "MyScripts.txt"
+	dataFile = "Super Powers Descriptor List.xlsx"
+	data = ReadSampleData(dataFile)
+	listId = 126890
+	script = GenerateControlledVocabularyScript(data, listId)
+	ShowScript(script)
+	WriteScript(file, script)
